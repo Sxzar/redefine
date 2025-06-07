0a2f1ca185c821c5641d2b8d44834c4be43bcded
Stefan Nicolau <contact.sxzar@gmail.com>
2025-06-07 14:02:26 +0300

    feat: add Navbar component and integrate into App; update styles for nav hover effect and adjust animation timing in AnimatedTitle

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -23,7 +23,7 @@ const AnimatedTitle = ({ title, containerClass }) => {
                     transform:
                         'translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)',
                     ease: 'power2.inOut',
-                    stagger: 0.05
+                    stagger: 0.02
                 },
                 0
             );
